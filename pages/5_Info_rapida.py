1b2de341f3f2a262ebe0a1648cbc50733fa943a5
agavirja <gaviriaalejandro@hotmail.com>
2023-02-11 15:29:46 -0500

    actualizar img

--- pages/5_Info_rapida.py
+++ pages/5_Info_rapida.py
@@ -90,4 +90,4 @@ if data.empty is False:
             with col2:
                 st.text_input(f'{value}',value=datashow[key].iloc[0])           
         conteo += 1
-    st.image(datashow['principal_img'].iloc[0],width=600)
+    st.image(datashow['principal_img'].iloc[0],width=300)
